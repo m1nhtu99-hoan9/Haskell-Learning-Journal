b16ad20e864de558d5a3efa35c381f960429279e
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-03-29 01:50:35 +0700

    update ch11

--- ch11-algebraic-datatypes/tree-exercise.hs
+++ ch11-algebraic-datatypes/tree-exercise.hs
@@ -5,4 +5,38 @@ module AlgebraicDataTypes.BinaryTreeADT where
                     deriving (Eq, Ord, Show)
   
   insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
+  -- initiate a new tree
+  insert' v Leaf                    = Node Leaf v Leaf
+  {--if inserted value equals existed value, nothing happens
+      else in case of LT: insert argument value on the left sub-binary tree
+      else in case of GT: insert argument value on the right sub-binary tree
+  --}   
+  insert' v (Node btLeft x btRight) = case compare v x of
+    EQ -> Node btLeft x btRight
+    LT -> Node (insert' v btLeft) x btRight
+    GT -> Node btLeft x (insert' v btRight)
+
+  mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+  mapTree _ Leaf = Leaf
+  mapTree f (Node btLeft x btRight) = 
+    Node (mapTree f btLeft) (f x) (mapTree f btRight)
   
+  testTree' :: BinaryTree Integer
+  testTree' = Node (Node Leaf 3 Leaf)
+                   1
+                   (Node Leaf 4 Leaf)
+  
+  expectedTree' :: BinaryTree Integer
+  expectedTree' =  Node (Node Leaf 8 Leaf)
+                   2
+                   (Node Leaf 16 Leaf)
+
+  testMapTree :: IO ()
+  testMapTree = 
+    if mapTree (2 ^) testTree' == expectedTree'
+    then putStrLn "Map Tree passed"
+    else putStrLn "Map Tree failed"
+
+  toListPreOrder :: BinaryTree Integer -> [Integer]
+  toListPreOrder Leaf                    = []
+  toListPreOrder (Node btLeft v btRight) = concat [[v], (toListPreOrder btLeft), (toListPreOrder btRight)]
