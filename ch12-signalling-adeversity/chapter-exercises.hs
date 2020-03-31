c58eea2da1ecc1ea4416aa07b5f039d43ae33ad6
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-03-31 17:03:19 +0700

    update ch12

--- ch12-signalling-adeversity/chapter-exercises.hs
+++ ch12-signalling-adeversity/chapter-exercises.hs
@@ -37,8 +37,58 @@ module Chap12.Exercises where
   isNothing :: Maybe t -> Bool
   isNothing Nothing = True
   isNothing _       = False
-
+    
+    -- `Maybe` catamorphism
   mayybee :: b -> (a -> b) -> Maybe a -> b
-  mayybee 
+  mayybee i f x = case isNothing x of
+    True  -> i
+    False -> let Just _x = x in f _x
+
+  justifise :: [Maybe a] -> [Maybe a]
+  justifise = filter (not . isNothing)
+
+  catMaybes :: [Maybe a] -> [a]
+  catMaybes = map (\j -> let Just j' = j in j') . justifise 
+
+  hasNothings :: [Maybe a] -> Bool
+  hasNothings = not . null . snd . break isNothing
+
+  flipMaybe :: [Maybe a] -> Maybe [a]
+  flipMaybe xs = case hasNothings xs of 
+    True  -> Nothing 
+    False -> Just (catMaybes xs) 
 
   -- @endregion
+
+  -- @region: Small library for `Either`
+  isEitherLeft :: Either a b -> Bool
+  isEitherLeft (Left _) = True
+  isEitherLeft _        = False
+
+  leftVal :: Either a b -> a
+  leftVal x = x' 
+    where Left x' = x
+
+  rightVal :: Either a b -> b
+  rightVal x = x'
+    where Right x' = x  
+  
+  lefts :: [Either a b] -> [a]
+  lefts = map leftVal . filter isEitherLeft
+
+  rights :: [Either a b] -> [b]
+  rights = map rightVal . filter (not . isEitherLeft)
+
+  halves :: [Either a b] -> ([a], [b])
+  halves es = (lefts es, rights es)
+  
+  -- @endregion 
+
+  -- @region: testing setup
+
+  type Number = Either Int String 
+  
+  listOfNums :: [Number]
+  listOfNums =  [Left 15, Right "eight", Left (negate 69), Right "one"]
+
+  -- @region
