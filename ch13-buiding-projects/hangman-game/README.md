6421284e72b22590e35cf0c872e85933911e7ed4
MinhTu Thomas Hoang <minhtu.hoang19@gmail.com>
2020-04-03 14:33:35 +0700

    update ch14

--- ch13-buiding-projects/hangman-game/README.md
+++ ch13-buiding-projects/hangman-game/README.md
@@ -5,3 +5,24 @@ Deadly simple demo project following guidance of chapter 13
 ### Notes on setting up project with Stack
 
 `$ stack new {projectName} simple`
+
+## My Errata on the book
+
+- On eBook page 883 (book page 835): `Prelude> :t Test.QuickCheck.arbitrary` instead of `Prelude> arbitrary`. _GHCi_ displays:
+
+```Haskell
+Test.QuickCheck.arbitrary
+  :: Test.QuickCheck.Arbitrary.Arbitrary a =>
+     Test.QuickCheck.Gen.Gen a
+```
+
+- On eBook page 884 (book page 836): similarly to the previous erratum:
+
+```Haskell
+Prelude> :t Test.QuickCheck.sample
+Test.QuickCheck.sample
+  :: Show a => Test.QuickCheck.Gen.Gen a -> IO ()
+Prelude> :t Test.QuickCheck.sample'
+Test.QuickCheck.sample'
+  :: Test.QuickCheck.Gen.Gen a -> IO [a]
+```
--- ch14-testing/addition/Addition.hs
+++ ch14-testing/addition/Addition.hs
@@ -1,13 +1,53 @@
 module Addition where
 import Test.Hspec
+import Test.QuickCheck
 import ProblemEulerOne
 
-sayHello :: IO ()
-sayHello = putStrLn "Hello!"
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+-- "cheat" a little so that we can get more Just value
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing) , 
+              (3, return (Just a)) ]
+
+  -- frequency :: [(Int, Gen a)] -> Gen a
+  
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c)
+ => Gen (a, b, c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a, b, c)
+
+qchkIsDividedByProp1 :: Int -> Int -> Bool
+qchkIsDividedByProp1 0 _ = True
+qchkIsDividedByProp1 x y = (==) (isDividedBy x y) (isDividedBy x (negate y))
+
+qchkIsDividedByProp2 :: Int -> Int -> Bool
+qchkIsDividedByProp2 0 _ = True
+qchkIsDividedByProp2 x y = (==) (isDividedBy x y) (isDividedBy x (y + x))
 
 main :: IO ()
 main = hspec $ do
-  describe "Test ProblemEulerOne" $ do
+  describe "Test Specification of ProblemEulerOne" $ do
     it "\"isDividedBy 5 455\" should be True" $ do
       (isDividedBy 5 455) `shouldBe` True
     it "\"isDividedBy 3 455\" should be False" $ do
@@ -24,6 +64,12 @@ main = hspec $ do
       (sumOfMultiples 10) `shouldBe` 33
     it "\"sumOfMultiples\" test 3" $ do
       (sumOfMultiples 1000) `shouldBe` 234168
+  describe "Test Properties of ProblemEulerOne" $ do
+    it "\"isDividedBy\" evaluates to the same result for (x, y) and (x, -y)" $ do
+      property $ qchkIsDividedByProp1 
+    it "\"isDividedBy\" evaluates to the same result for (x, y) and (x, y+x)" $ do
+      property $ qchkIsDividedByProp2
+    --it "\"sumOfMultiples\" function is increasing"
     
 
     
